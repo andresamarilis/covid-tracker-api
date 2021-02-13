e72050c294453c68bb8621e93651ad55aaafa84b
andresamarilis <andres.amarilis@student.uts.edu.au>
2021-02-13 22:48:37 +1100

    complete

---
+++ index.js
@@ -0,0 +1,57 @@
+
+
+    init()
+
+ function init(){
+
+   let url = "https://api.covid19api.com/summary"
+
+
+
+   $.get(url, function(data){
+
+       //console.log(data.Countries[8].TotalConfirmed)//
+      // console.log(data.Countries[8])//
+
+       data = `<td> ${data.Countries[8].NewConfirmed} </td>
+       <td> ${data.Countries[8].NewDeaths} </td>
+       <td> ${data.Countries[8].NewRecovered} </td>`
+
+      
+
+       
+       $("#data").html(data)
+     
+
+   });
+
+
+   $.get(url, function(dataTwo){
+
+   
+    dataTwo = `<td> ${dataTwo.Countries[8].TotalConfirmed} </td>
+    <td> ${dataTwo.Countries[8].TotalDeaths} </td>
+    <td> ${dataTwo.Countries[8].TotalRecovered} </td>`
+ 
+
+    $("#dataTwo").html(dataTwo)
+
+})
+
+
+}
+
+let refreshData = () => {
+    clearData()
+    init()
+}
+
+let clearData = () => {
+    $("#data").empty()
+    $("#dataTwo").empty()
+      
+}
+
+
+
+
